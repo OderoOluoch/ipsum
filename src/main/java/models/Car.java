003ea1c233cdd6084b34512baac272ba8dac8d3d
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-22 13:08:00 +0300

    setters

--- src/main/java/models/Car.java
+++ src/main/java/models/Car.java
@@ -20,19 +20,19 @@ public  Car(String car_name, int car_id, int parking_slot_id){
         return car_id;
     }
 
-//    public int getParking_slot_id() {
-//        return parking_slot_id;
-//    }
-//
-//    public void setCar_name(String car_name) {
-//        this.car_name = car_name;
-//    }
-//
-//    public void setCar_id(int car_id) {
-//        this.car_id = car_id;
-//    }
-//
-//    public void setParking_slot_id(int parking_slot_id) {
-//        this.parking_slot_id = parking_slot_id;
-//    }
+    public int getParking_slot_id() {
+        return parking_slot_id;
+    }
+
+    public void setCar_name(String car_name) {
+        this.car_name = car_name;
+    }
+
+    public void setCar_id(int car_id) {
+        this.car_id = car_id;
+    }
+
+    public void setParking_slot_id(int parking_slot_id) {
+        this.parking_slot_id = parking_slot_id;
+    }
 }

c1057b26fc5f0a7d9237414dc88db8968199e3c5
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-22 13:11:47 +0300

    equals

--- src/main/java/models/Car.java
+++ src/main/java/models/Car.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Car {
     private String car_name;
     private int car_id;
@@ -12,6 +14,7 @@ public  Car(String car_name, int car_id, int parking_slot_id){
     this.parking_slot_id = parking_slot_id;
 }
 
+//Getters
     public String getCar_name() {
         return car_name;
     }
@@ -24,6 +27,9 @@ public  Car(String car_name, int car_id, int parking_slot_id){
         return parking_slot_id;
     }
 
+
+    //setters
+
     public void setCar_name(String car_name) {
         this.car_name = car_name;
     }
@@ -34,5 +40,23 @@ public  Car(String car_name, int car_id, int parking_slot_id){
 
     public void setParking_slot_id(int parking_slot_id) {
         this.parking_slot_id = parking_slot_id;
+    }
+
+    //equals & hashCodes
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return car_id == car.car_id &&
+                parking_slot_id == car.parking_slot_id &&
+                Objects.equals(car_name, car.car_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(car_name, car_id, parking_slot_id);
     }
 }

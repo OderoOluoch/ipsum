ac2d91d4ccc4bc834fa1aa6b8e23ddda420b979b
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-06-22 13:01:31 +0300

    constractors

--- src/main/java/dao/CarDao.java
+++ src/main/java/dao/CarDao.java
@@ -1,4 +1,10 @@
 package dao;
+import models.Car;
+import java.util.List;
 
 public interface CarDao {
+    void add (Car car);
+
+    List<Car> getAll();
+
 }
--- src/main/java/models/Car.java
+++ src/main/java/models/Car.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Car {
+    private String car_name;
+    private int car_id;
+    private  int parking_slot_id;
+
+
+public  Car(String car_name, int car_id, int parking_slot_id){
+    this.car_name = car_name;
+    this.car_id = car_id;
+    this.parking_slot_id = parking_slot_id;
+}
+
+
 }

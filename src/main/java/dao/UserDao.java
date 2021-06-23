044bb9b72548de601af3379524b7500d717b69f0
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-24 00:50:18 +0300

    add changes

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> getAllUsers();
+    void addUser(User user);
+    int findUserById(int id);
+
+    void updateUser(User user, int id, String name, int carId, int parkingId);
+
+    void deleteById(int id);
+    void clearAll();
 }
---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,3 @@
+SET MODE PostgresSQL;
+CREATE DATABASE
+CREATE TABLE users(id serial PRIMARY KEY, name varchar, carId int, parkingId int);

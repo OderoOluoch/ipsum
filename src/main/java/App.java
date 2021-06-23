f11bfdcf2ff89df357334f20972ddbb136e49f22
OderoOluoch <oderoluoch@gmail.com>
2021-06-24 01:30:31 +0300

    Fixing the merge head conflict

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,39 @@
 import com.google.gson.Gson;
-<<<<<<< HEAD
-import dao.CarDao;
 import dao.Sql2oCarDao;
-import exception.ApiException;
-import org.sql2o.Connection;
-import org.sql2o.Sql2o;
-import models.Car;
-
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import static spark.Spark.*;
-
-
-=======
 import dao.Sql2oParkingSlotDao;
+import exception.ApiException;
+import models.Car;
 import models.ParkingSlot;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
-import static spark.Spark.*;
 
->>>>>>> 24461736554a375f46d22231012b2f107203ef25
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class App {
+    Sql2oParkingSlotDao parkingSlotDao;
+
+
+    Gson gson = new Gson();
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Sql2oCarDao sql20CarDao;
-        Connection conn;
-        Gson gson = new Gson();
 
-        String connectionString = "jdbc:postgresql://localhost:5432/car_park";
-        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Access");
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+//
+//        String connectionString = "jdbc:postgresql://localhost:5432/car_park";
+//        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Access");
+
+        Sql2oCarDao sql20CarDao;
+        Gson gson = new Gson();
+        Connection conn;
+        Sql2oParkingSlotDao parkingSlotDao;
+        parkingSlotDao = new Sql2oParkingSlotDao(sql2o);
+        conn = sql2o.open();
+
+
+
 
         sql20CarDao = new Sql2oCarDao (sql2o) {
 
@@ -49,7 +51,7 @@ public class App {
             return gson.toJson(car);
         });
 
-        get("/users", "application/json",(req, res) -> {
+        get("/cars", "application/json",(req, res) -> {
             if(sql20CarDao.getAll().size() < 1){
                 throw new ApiException(404, String.format("notAvailableMsg","Cars"));
             }
@@ -57,17 +59,7 @@ public class App {
             return gson.toJson(sql20CarDao.getAll());
         });
 
-=======
-        Sql2oParkingSlotDao parkingSlotDao;
 
-        Connection conn;
-        Gson gson = new Gson();
-
-        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
-        Sql2o sql2o = new Sql2o(connectionString, "", "");
-
-        parkingSlotDao = new Sql2oParkingSlotDao(sql2o);
-        conn = sql2o.open();
 
         post("/slots/new", "application/json", (req, res) -> {
             ParkingSlot slot = gson.fromJson(req.body(), ParkingSlot.class);
@@ -81,7 +73,7 @@ public class App {
             res.type("application/json");
             return gson.toJson(parkingSlotDao.getAll());//send it back to be displayed
         });
->>>>>>> 24461736554a375f46d22231012b2f107203ef25
+
 
 //        get("/restaurants/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
 //            res.type("application/json");

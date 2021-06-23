310cffac7b0321a523d0798afcace35f9e8042b2
OderoOluoch <oderoluoch@gmail.com>
2021-06-23 07:48:11 +0300

    Parking slot interface in place

--- src/main/java/dao/ParkingSlotDao.java
+++ src/main/java/dao/ParkingSlotDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.ParkingSlot;
+
+import java.util.List;
+
 public interface ParkingSlotDao {
+    //create
+    void add(ParkingSlot slot);
+
+    //read
+    List<ParkingSlot> getAll();
+    List<ParkingSlot> getAllDepartmentsForNews(int id);
+
+    //update
+    //omit for now
+
+    //delete
+    void deleteById(int id);
+
+    void clearAll();
 }

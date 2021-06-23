a5400050b4a873c60393a6a29297f9f29360d157
OderoOluoch <oderoluoch@gmail.com>
2021-06-23 08:18:44 +0300

    Parking slot END points in prigress

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,30 @@
+import com.google.gson.Gson;
+import dao.Sql2oParkingSlotDao;
+import models.ParkingSlot;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
 public class App {
 
     public static void main(String[] args) {
+        Sql2oParkingSlotDao parkingSlotDao;
 
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+
+        parkingSlotDao = new Sql2oParkingSlotDao(sql2o);
+        conn = sql2o.open();
+
+        post("/slots/new", "application/json", (req, res) -> {
+            ParkingSlot slot = gson.fromJson(req.body(), ParkingSlot.class);
+            parkingSlotDao.add(slot);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(slot);
+        });
     }
 }
